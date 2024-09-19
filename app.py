b7deea3f6de80588e307c12359709992ed737206
LuIS Javier Cruz Trejo <cruzjavi092@gmail.com>
2024-09-18 18:07:36 -0600

    2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jaime:l58ciipdmdX3AsPE319boApTXNzfYoyj@dpg-crd2qu3tq21c73csl160-a.oregon-postgres.render.com:5432/test_bbaj'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://luis:aslgrAObLF81et9BTIgIPNLXh7UAAT4r@dpg-crfn0g23esus73f3frpg-a/javier_r5d0'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
